0fc81bdfe852ee6dc6548cb27b7433ece20aaccc
SIGMOID <1535899184@qq.com>
2022-11-08 13:31:27 +0800

    Create dp.py
    this is  a simple dp pytho application.

---
+++ dp/dp.py
@@ -0,0 +1,63 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+"""
+@Email: zl2272001@gmail.com
+@Time: 2022/11/4
+@target: Using DP modle to train data
+"""
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+
+# Parameters and DataLoaders
+input_size = 5
+output_size = 2
+batch_size = 40
+data_size = 100
+
+# Device
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Dummy Dataset
+class RandomDataset(Dataset):
+    def __init__(self, size, length):
+        self.len = length
+        self.data = torch.randn(length, size)
+
+    def __getitem__(self, index):
+        return self.data[index]
+
+    def __len__(self):
+        return self.len
+# dataloader
+rand_loader = DataLoader(dataset=RandomDataset(input_size, data_size),
+                         batch_size=batch_size, shuffle=True)
+
+# demo model - linear operation
+class Model(nn.Module):
+    def __init__(self, input_size, output_size):
+        super(Model, self).__init__()
+        self.fc = nn.Linear(input_size, output_size)
+
+    def forward(self, input):
+        output = self.fc(input)
+        print(f"In Model:", "input-",input.shape, input.device,
+              " output:", output.size(), output.device)
+        return output
+    
+# Using nn.DataParallel to parallel data, mul GPUs (4) 
+model = Model(input_size, output_size)
+if torch.cuda.device_count() > 1:
+  print("Let's use", torch.cuda.device_count(), "GPUs!", f"batch_size = {batch_size}")
+  # dim = 0 [40, xxx] -> [10, ...], [10, ...], [10, ...] ,[10,...]on 4 GPUs
+  model = nn.DataParallel(model)
+model.to(device)
+
+# train data
+# print the size of input tensor and output tensor
+for i, data in enumerate(rand_loader):
+    input = data.to(device)
+    output = model(input)
+    print(f"[{i}]:", "Outside: input size", input.size(),
+          " output_size", output.size())
