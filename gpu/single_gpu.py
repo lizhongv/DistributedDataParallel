5538c29ec977ed7738be3e2151b3f16e752961be
SIGMOID <1535899184@qq.com>
2022-11-14 16:27:06 +0800

    Update single_gpu.py
    a simple piece of code for training with a single GPU.

--- gpu/single_gpu.py
+++ gpu/single_gpu.py
@@ -3,14 +3,14 @@
 """
 @Email: zl2272001@gmail.com
 @Time: 2022/11/4
-@target: Using single GPU
+@target: Using single GPU to train data
 """
 
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
 
-# Parameters and DataLoaders
+# Parameters 
 input_size = 5
 output_size = 2
 batch_size = 40
@@ -19,7 +19,7 @@ data_size = 100
 # Device
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
-# Dummy Dataset
+# Dataset operations
 class RandomDataset(Dataset):
     def __init__(self, size, length):
         self.len = length
@@ -30,11 +30,12 @@ class RandomDataset(Dataset):
 
     def __len__(self):
         return self.len
-# dataloader
+    
+# Dataloader
 rand_loader = DataLoader(dataset=RandomDataset(input_size, data_size),
                          batch_size=batch_size, shuffle=True)
 
-# demo model - linear operation
+# Model  linear neural network 
 class Model(nn.Module):
     def __init__(self, input_size, output_size):
         super(Model, self).__init__()
@@ -46,15 +47,20 @@ class Model(nn.Module):
               " output:", output.size(), output.device)
         return output
    
+# Loading model
 model = Model(input_size, output_size)
 if torch.cuda.device_count() > 1:
   print("Let's use", torch.cuda.device_count(), "GPUs!", f"batch_size = {batch_size}")
 model.to(device)
 
-# training data
-# print the size of input tensors and output tensors 
+# Training data
 for i, data in enumerate(rand_loader):
-    input = data.to(device)
+    # Loading input data
+    input = data.to(device)  
+    
+    # Predict
     output = model(input)
+    
+    # Print the size of input tensors and output tensors 
     print(f"[{i}]:", "Outside: input size", input.size(),
           " output_size", output.size())
